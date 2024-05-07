d65f456450f6e793f9f964c5b93854c48c498487
jannatfaisal <118313474+jannatfaisal@users.noreply.github.com>
2024-05-07 16:35:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,112 @@
+// alert("jannatfaisal")
+
+// automatically declared
+//    x=30;
+//    y=20;
+//   u = x - y;
+// document.getElementById("demo").innerHTML =
+// "The Value of Z is:   "+ u;
+
+// using var
+
+// var a=30;
+// var b=40;
+// var c=30;
+// var d=10
+// var f=a-b+c+d;
+
+// document.getElementById("head").innerHTML =
+// "The answer is: " +f;
+
+// using const
+
+// const e=110;
+// const f=20;
+// const d=e-f;
+
+// document.getElementById("hh").innerHTML =
+// "The  is: " -d; 
+
+
+// document.getElementByconsole("h").innerHTML= "hello jannat"
+// console.log="hellojannat"
+
+
+// Arithmetic Operators Example
+
+// let a = 3; // a ko 3 ke barabar set karte hain.
+// let x = (100 + 50) * a; // Yahan, 100 aur 50 ko add kar ke phir a ke saath multiply kiya jata hai.
+// console.log(x)
+
+// Assignment
+
+// let x = 10;
+// x += 5;
+// console.log(x)
+
+
+// comparison
+
+// let text1="a"
+// let text2="b"
+// let result =text1 <  text2;
+// console.log(result)
+
+////"<" ek comparison operator hai jo do values ko compare karta hai aur batata hai ke left side wali value right side wali value se choti hai ya nahi.
+
+// Detail:
+
+// Isko "Less Than" ya "Chhota Hai" kehte hain.
+// Agar left side wali value right side wali value se choti hai, toh "<" true (ya 1) ko represent karega.
+// Agar left side wali value right side wali value se badi ya equal hai, toh "<" false (ya 0) ko represent karega.
+// Iska istemal conditional statements mein, jaise "if" aur "while" mein hota hai, jahan humein kisi condition ko check karna hota hai.
+
+
+
+// let text1 = "20";
+// let text2 = "5";
+// let result = text1 > text2;
+// console.log(result)
+
+// ">" ek comparison operator hai jo do values ko compare karta hai aur batata hai ke left side wali value right side wali value se badi hai ya nahi.
+
+// Detail:
+// - Isko "Greater Than" ya "Bada Hai" kehte hain.
+// - Agar left side wali value right side wali value se badi hai, toh ">" true (ya 1) ko represent karega.
+// - Agar left side wali value right side wali value se choti ya equal hai, toh ">" false (ya 0) ko represent karega.
+// - Iska istemal conditional statements mein, jaise "if" aur "while" mein hota hai, jahan humein kisi condition ko check karna hota hai.
+
+
+// concatenate
+
+// let text1="jannat";
+// let text2="faisal";
+// let text3 ="is";
+// let text4 ="a";
+// let text5 ="Web";
+// let text6="developer"
+// let result =   text1 +  "  " +text2+ " " +text3 +"   " +  text4+   "  " +text5+"   "+text6+" "
+
+// console.log(result);
+
+/* <p>The assignment operator += can concatenate strings.</p> */
+
+// let text1="what a very";
+// text1+="nice day";
+// console.log(text1)
+
+
+//  text1="Web";
+//  text1+="developer";
+// console.log(text1);
+
+
+// Adding Strings and Numbers
+// Adding two numbers, will return the sum, but adding a number and a string will return a string:
+
+
+// let x=10+10;
+// let y ="5"+5;
+// let z= "hello" +5;
+// console.log(x+y+z);
+
